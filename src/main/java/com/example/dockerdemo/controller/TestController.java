0797ca487ff18f1b54e6c0b51639fc0bbbbc1a1b
fhyang <fhyangyuan@163.com>
2022-08-31 11:13:42 +0800

    init project

---
+++ src/main/java/com/example/dockerdemo/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.example.dockerdemo.controller;
+
+import netscape.javascript.JSObject;
+import netscape.javascript.JSUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author YangYuan
+ * @date 2022/8/31
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/a")
+    public Object getTest(){
+        Map<String, Object> data = new HashMap<>();
+        data.put("name","张三");
+        data.put("age",20);
+        return data;
+    }
+}
